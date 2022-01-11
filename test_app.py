5c543c82d3103ff7745d910c7db74f57d4b28851
Àngel Fernández <angel.fernandez@holaluz.com>
2022-01-11 16:07:57 +0100

    Fix hour tests

--- test_app.py
+++ test_app.py
@@ -35,17 +35,17 @@ def test_get_nth_big_word():
     assert get_nth_big_word(raw_response["paraules"].keys(), 3) == "deessa"
 
 
-@freeze_time("2021-12-21T09:01:22")
+@freeze_time("2021-12-21T08:01:22")
 def test_get_position_by_datetime_0():
     assert get_position_by_datetime() == 0
 
 
-@freeze_time("2021-12-21T10:32:54")
+@freeze_time("2021-12-21T09:32:54")
 def test_get_position_by_datetime_4():
     assert get_position_by_datetime() == 4
 
 
-@freeze_time("2021-12-21T11:59:11")
+@freeze_time("2021-12-21T10:59:11")
 def test_get_position_by_datetime_8():
     assert get_position_by_datetime() == 8
 
